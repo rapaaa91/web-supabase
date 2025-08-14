59e1abdc768fe0fcfb94cf45f109a9b91f1d395b
rapaaa91 <rafakiki36367373@gmail.com>
2025-08-14 12:02:21 +0700

    Initial commit

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// Ganti URL & ANON KEY Supabase mu
+const SUPABASE_URL = "https://wlpkmvrjwsslknjhqwvd.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndscGttdnJqd3NzbGtuamhxd3ZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxNDQ5ODIsImV4cCI6MjA3MDcyMDk4Mn0.-k51rh0XmVt4c5dj6-DnD-JjD29y8ZYoB2erjAuyFc4";
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
